b5c96224c74a8e4642796088c04457b4819fec37
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-02-01 07:53:39 +0900

    Bump up version v0.1.5

--- mediumctl.rb
+++ mediumctl.rb
@@ -1,16 +1,16 @@
 class Mediumctl < Formula
   desc 'CLI tool for publishing an article to Medium'
   homepage 'https://github.com/moutend/mediumctl'
-  version 'v0.1.4'
+  version 'v0.1.5'
 
   if Hardware::CPU.is_64_bit?
-    @@binname = 'mediumctl_darwin_amd64_v0.1.4'
-    url 'https://github.com/moutend/mediumctl/releases/download/v0.1.4/' << @@binname
-    sha256 '74c39e192dfa6df966335a3024f0c5595748885aae62a20739fb4789c3f5b625'
+    @@binname = 'mediumctl_darwin_amd64_v0.1.5'
+    url 'https://github.com/moutend/mediumctl/releases/download/v0.1.5/' << @@binname
+    sha256 'c07161d889ffcea3c7b0a1c92eedeb5a6ad4bd352dc89caa067afa5ee7316805'
   else
-    @@binname = 'mediumctl_darwin_386_v0.1.4'
-    url 'https://github.com/moutend/mediumctl/releases/download/v0.1.4/' << @@binname
-    sha256 'a47ab2758cffcc738d508849b964df7aa919188400045cbb5f90f30e5cfa803e'
+    @@binname = 'mediumctl_darwin_386_v0.1.5'
+    url 'https://github.com/moutend/mediumctl/releases/download/v0.1.5/' << @@binname
+    sha256 '5971f0d7e6cb80a90471d776c12867c1cd15292e737206edb7f7496613961057'
   end
 
   def install
