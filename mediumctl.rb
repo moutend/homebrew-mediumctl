36ddc231aa3da1dd47f1267aa4a1bca5ae131d06
Yoshiyuki Koyanagi <moutend@gmail.com>
2019-05-04 07:54:30 +0900

    Fix wrong sha256

--- mediumctl.rb
+++ mediumctl.rb
@@ -6,11 +6,11 @@ class Mediumctl < Formula
   if Hardware::CPU.is_64_bit?
     @@binname = 'mediumctl_darwin_amd64_v0.4.0'
     url 'https://github.com/moutend/mediumctl/releases/download/v0.4.0/' << @@binname
-    sha256 '708ab8f7f9872004ec4af2d95d70da72f565635b3fc757f8057e49a5a59569fe '
+    sha256 '708ab8f7f9872004ec4af2d95d70da72f565635b3fc757f8057e49a5a59569fe'
   else
     @@binname = 'mediumctl_darwin_386_v0.4.0'
     url 'https://github.com/moutend/mediumctl/releases/download/v0.4.0/' << @@binname
-    sha256 '41320fbe77e0b539cadb3e4c98581c502b44b7748fccb5b1e5b2322d1e9a4bca '
+    sha256 '41320fbe77e0b539cadb3e4c98581c502b44b7748fccb5b1e5b2322d1e9a4bca'
   end
 
   def install
